09b2857dd98bcd23024290f1ac73354510e2c6cd
Jesse Paris <jparis@gmail.com>
2019-07-19 00:10:23 +0200

    adding tests

--- test/testFactory.js
+++ test/testFactory.js
@@ -2,7 +2,6 @@ var nock = require('nock');
 var qrsInteractMain = require('../qrsInteract');
 var request = require('request');
 
-
 // test setup
 
 var generateXrfKey = function() {
@@ -29,120 +28,119 @@ var xrfkeyParam = "xrfkey=" + xrfkey;
 var qrsInteractInstance = new qrsInteractMain("http://test.factory", "", "", xrfkeyParam, request);
 
 
-try {
-    // test case 1
 
-    var test1Return = {
-        buildVersion: '3.0.2.0',
-        buildDate: '9/20/2013 10:09:00 AM',
-        databaseProvider: 'Devart.Data.PostgreSql',
-        nodeType: 1,
-        schemaPath: 'About'
-    };
+// test case 1
 
-    var scope = nock('http://test.factory')
-        .get('/qrs/about' + '?' + xrfkeyParam)
-        .reply(200, test1Return);
+var test1Return = {
+    buildVersion: '3.0.2.0',
+    buildDate: '9/20/2013 10:09:00 AM',
+    databaseProvider: 'Devart.Data.PostgreSql',
+    nodeType: 1,
+    schemaPath: 'About'
+};
 
-    qrsInteractInstance.Get('about').then(function(result) {
-        if (JSON.stringify(result.body) != JSON.stringify(test1Return)) {
-            throw "testcase 1 failed - Get returned wrong result.";
-        } else {
-            console.log("testcase 1 passed - Get");
-        }
-    });
+var scope = nock('http://test.factory')
+    .get('/qrs/about' + '?' + xrfkeyParam)
+    .reply(200, test1Return);
 
-
-
-    // test case 2
-
-    var test2Return = {
-        id: "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
-        createdDate: "2016-09-28T16:20:39.982Z",
-        modifiedDate: "2016-09-28T16:20:39.982Z",
-        modifiedByUserName: "INTERNAL\\sa_api",
-        name: "tagTest",
-        privileges: null,
-        schemaPath: "Tag"
-    };
-
-    var scope = nock('http://test.factory')
-        .post('/qrs/tag' + '?' + xrfkeyParam, {
-            "id": "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
-            "name": "tagTest",
-            "privileges": null
-        })
-        .reply(201, test2Return);
-
-    qrsInteractInstance.Post('tag', JSON.stringify({
-        id: "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
-        name: "tagTest",
-        privileges: null
-    }), 'json').then(function(result) {
-        if (JSON.stringify(result.body) != JSON.stringify(test2Return)) {
-            throw "testcase 2 failed - Post returned wrong result.";
-        } else {
-            console.log("testcase 2 passed - Post");
-        }
-    });
+qrsInteractInstance.Get('about').then(function(result) {
+    if (JSON.stringify(result.body) != JSON.stringify(test1Return)) {
+        throw "testcase 1 failed - Get returned wrong result.";
+    } else {
+        console.log("testcase 1 passed - Get");
+    }
+});
 
 
 
-    // test case 3
+// test case 2
 
-    var test3Return = [{
-        id: "1234e69a-d2fe-4d1a-bc64-52c5b4232e87",
-        createdDate: "2016-09-28T16:20:39.982Z",
-        modifiedDate: "2016-09-28T16:20:39.982Z",
-        modifiedByUserName: "INTERNAL\\sa_api",
-        name: "tag1",
-        privileges: null,
-        schemaPath: "Tag"
-    }, {
-        id: "4321e69a-d2fe-4d1a-bc64-52c5b4232e87",
-        createdDate: "2016-09-28T16:20:39.982Z",
-        modifiedDate: "2016-09-28T16:20:39.982Z",
-        modifiedByUserName: "INTERNAL\\sa_api",
-        name: "tag2",
-        privileges: null,
-        schemaPath: "Tag"
-    }];
+var test2Return = {
+    id: "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
+    createdDate: "2016-09-28T16:20:39.982Z",
+    modifiedDate: "2016-09-28T16:20:39.982Z",
+    modifiedByUserName: "INTERNAL\\sa_api",
+    name: "tagTest",
+    privileges: null,
+    schemaPath: "Tag"
+};
 
-    var scope = nock('http://test.factory')
-        .get('/qrs/tag' + '?' + xrfkeyParam)
-        .reply(200, test3Return);
+var scope = nock('http://test.factory')
+    .post('/qrs/tag' + '?' + xrfkeyParam, {
+        "id": "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
+        "name": "tagTest",
+        "privileges": null
+    })
+    .reply(201, test2Return);
 
-    qrsInteractInstance.Get('tag').then(function(result) {
-        return result.body[0].name;
-    }).then(function(name) {
-        if (name != "tag1") {
-            throw "testcase 3 failed - Get returned wrong result.";
-        } else {
-            console.log("testcase 3 passed - Get array");
-        }
-    });
+qrsInteractInstance.Post('tag', JSON.stringify({
+    id: "2454e69a-d2fe-4d1a-bc64-52c5b4232e87",
+    name: "tagTest",
+    privileges: null
+}), 'json').then(function(result) {
+    if (JSON.stringify(result.body) != JSON.stringify(test2Return)) {
+        throw "testcase 2 failed - Post returned wrong result.";
+    } else {
+        console.log("testcase 2 passed - Post");
+    }
+});
 
 
 
-    // test case 4
+// test case 3
 
-    var test4Return = "someStringBuffer";
+var test3Return = [{
+    id: "1234e69a-d2fe-4d1a-bc64-52c5b4232e87",
+    createdDate: "2016-09-28T16:20:39.982Z",
+    modifiedDate: "2016-09-28T16:20:39.982Z",
+    modifiedByUserName: "INTERNAL\\sa_api",
+    name: "tag1",
+    privileges: null,
+    schemaPath: "Tag"
+}, {
+    id: "4321e69a-d2fe-4d1a-bc64-52c5b4232e87",
+    createdDate: "2016-09-28T16:20:39.982Z",
+    modifiedDate: "2016-09-28T16:20:39.982Z",
+    modifiedByUserName: "INTERNAL\\sa_api",
+    name: "tag2",
+    privileges: null,
+    schemaPath: "Tag"
+}];
 
-    var scope = nock('http://test.factory')
-        .get('/tempcontent' + '?' + xrfkeyParam)
-        .reply(200, test1Return);
+var scope = nock('http://test.factory')
+    .get('/qrs/tag' + '?' + xrfkeyParam)
+    .reply(200, test3Return);
+
+qrsInteractInstance.Get('tag').then(function(result) {
+    return result.body[0].name;
+}).then(function(name) {
+    if (name != "tag1") {
+        throw "testcase 3 failed - Get returned wrong result.";
+    } else {
+        console.log("testcase 3 passed - Get array");
+    }
+});
 
 
-    qrsInteractInstance.Get('tempcontent').then(function(result) {
-        if (result.body != test4Return) {
-            throw "testcase 4 failed - Get returned wrong result.";
-        } else {
-            console.log("testcase 4 passed - Get");
-        }
-    });
 
+// test case 4
 
+var test4Return = "someStringBuffer";
 
-} catch (error) {
-    process.exit(1);
-}
+var scope1 = nock('http://test.factory')
+    .get('/tempcontent' + '?' + xrfkeyParam)
+    .reply(200, test1Return);
+
+var scope2 = nock('http://test.factory')
+    .get('/qrs/tempcontent' + '?' + xrfkeyParam)
+    .reply(404, "Not Found");
+
+qrsInteractInstance.Get('tempcontent').then(function(result) {
+    if (result.body != test4Return) {
+        throw "testcase 4 failed - Get returned wrong result.";
+    } else {
+        console.log("testcase 4 passed - Get");
+    }
+}).catch(function(err) {
+    throw "testcase 4 failed - Get returned wrong result.";
+});
